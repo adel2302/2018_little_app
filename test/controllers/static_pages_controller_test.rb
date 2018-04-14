d120f3c8a9e43bddee5549afc60c04d47d16c9ea
Adel Chouanine <a.chouanine@hotmail.fr>
2018-04-14 23:08:12 +0200

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | simple app"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | simple app"
   end
 
+  test "should get about" do
+  	get :about
+  	assert_response :success
+  	assert_select "title", "About | simple app"
+  end
 end
